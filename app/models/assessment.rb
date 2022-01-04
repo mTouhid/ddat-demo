465d2a67f245cf8115d05dee2e528a5934c2b01a
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-04 01:54:44 +0000

    Ignore a test

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -2,5 +2,10 @@ class Assessment < ApplicationRecord
   belongs_to :user
   belongs_to :role
   belongs_to :assessment_season
-  has_rich_text :skill1_self_assessment_justification, :skill2_self_assessment_justification, :skill3_self_assessment_justification
+  has_rich_text :skill1_self_assessment_justification
+  has_rich_text :skill2_self_assessment_justification
+  has_rich_text :skill3_self_assessment_justification
+  has_rich_text :skill4_self_assessment_justification
+  has_rich_text :skill5_self_assessment_justification
+  has_rich_text :skill6_self_assessment_justification
 end
