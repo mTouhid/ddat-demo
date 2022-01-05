1bc8ba55c34e2747fb3be58473e6474d96ace874
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-05 16:59:44 +0000

    Add user profile section with list of assessments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def nav_link_class(some_path)
+    if current_page?(some_path)
+      {class: "nav-link active"}
+    else
+      {class: "nav-link"}
+    end
+  end
 end
